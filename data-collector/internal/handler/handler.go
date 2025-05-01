4b195f721c465cdcdadee44f21d33de531f294de
Uchiman <ismailcelebi7@gmail.com>
2025-05-01 18:03:50 +0300

    db service moved to service path

--- data-collector/internal/handler/handler.go
+++ data-collector/internal/handler/handler.go
@@ -3,40 +3,12 @@ package handler
 import(
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/uchimann/air_pollution_project/data-collector/internal/repository"
-	"github.com/uchimann/air_pollution_project/data-collector/internal/model"
+
+	"github.com/uchimann/air_pollution_project/data-collector/internal/service"
 )
 
 func SetRoutes(app *fiber.App) {
 	apiRouter := app.Group("/api")
    
-	apiRouter.Post("/pollution", AddPollutionData)
+	apiRouter.Post("/pollution", service.AddPollutionData)
 }
-
-
-func AddPollutionData(ctx *fiber.Ctx) error {
-	var pollution model.PollutantDataInput
-	var err = ctx.BodyParser(&pollution)
-
-	if err != nil {
-	 return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-	  "data":  nil,
-	  "error": "Invalid request",
-	 })
-	}
-
-	err = repository.DB.Create(&pollution).Error
-	if err != nil {
-	 return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-	  "data":  nil,
-	  "error": "Create operation failed",
-	 })
-	}
-
-	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
-	 "data":  pollution,
-	 "error": nil,
-	})
-}
-   
-   
---
+++ data-collector/internal/service/pollution_input.go
@@ -0,0 +1,33 @@
+package service
+
+import(
+	"github.com/gofiber/fiber/v2"
+	"github.com/uchimann/air_pollution_project/data-collector/internal/model"
+	"github.com/uchimann/air_pollution_project/data-collector/internal/repository"
+)
+
+
+func AddPollutionData(ctx *fiber.Ctx) error {
+	var pollution model.PollutantDataInput
+	var err = ctx.BodyParser(&pollution)
+
+	if err != nil {
+	 return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+	  "data":  nil,
+	  "error": "Invalid request",
+	 })
+	}
+
+	err = repository.DB.Create(&pollution).Error
+	if err != nil {
+	 return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+	  "data":  nil,
+	  "error": "Create operation failed",
+	 })
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
+	 "data":  pollution,
+	 "error": nil,
+	})
+}
